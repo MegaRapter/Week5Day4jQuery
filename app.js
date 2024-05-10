b17e1178f74a787e71c1a9195c154d926d7dac1c
Texas R3X <texasr3xphone@gmail.com>
2024-05-10 16:31:33 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+$(".navItem").css("color", "#fff");
+$("#nav").css("background", "#145");
+
+$("li:even").css("background", "#ccc");
+$("li:odd").css("background", "#aaa");
+
+$("input").val("MegaRapter");   
